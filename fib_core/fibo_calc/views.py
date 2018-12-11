c43fb77752de3cf649e3ec23985597bc7a654cf4
TheVerin <tomekbeben95@gmail.com>
2018-12-11 17:36:26 +0100

    Some problems - last stable version

--- fib_core/fibo_calc/views.py
+++ fib_core/fibo_calc/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import FibResults
 
-# Create your views here.
+
+def calculation(n: int):
+    sl = [0,1]
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        for i in range(n - 1):
+            sl.append(sl[-1] + sl[-2])
+        return sl[-1]
+
+
+
